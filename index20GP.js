ae82387e9af46086901629ad7cffabd6350bcd0d
briaar <briarusss@gmail.com>
2023-08-23 15:32:22 -0400

    made text changes

---
+++ index20GP.js
@@ -0,0 +1,19 @@
+
+/* 
+
+const root = document.getElementById("root");
+
+const h1 = document.createElement("h1");
+h1.innerHTML = "MONA LISA";
+
+root.appendChild(h1);
+
+const img = document.createElement("img");
+image.src = './assets/mona_lisa.jpg';    
+root.appendChild(img);
+
+const paragraph = document.createElement("p");
+paragraph.innerText = "The Mona Lisa is a famous painting.";
+root.appendChild(p);
+
+  */
